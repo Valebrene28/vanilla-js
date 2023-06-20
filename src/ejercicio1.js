806103175d30e7fdd03bb070709e99c020e3b328
Vale Brenes <valebrenes2003@gmail.com>
2023-06-20 15:05:53 -0600

    Actualizacion 3pm

--- src/ejercicio1.js
+++ src/ejercicio1.js
@@ -1,8 +1,10 @@
 let contador = {
   valor: 0,
   siguiente: function () {
+    this.valor =this.valor+1;
+    return this.valor;
     // Inserte el código aquí
   },
 };
-
+console.log(contador.siguiente())
 export { contador }
--- src/ejercicio2.js
+++ src/ejercicio2.js
@@ -1,7 +1,16 @@
+import { contador } from "./ejercicio1";
+
 class Contador {
+  valor = 0;
+
+  siguente() {
+
+    this.valor += 1;
+
+  }
   // Inserte el código aquí
 }
 
 // Inserte el código aquí
-
+const instancia = new contador();
 export { instancia }
