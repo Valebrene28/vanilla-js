fff46f5b90e578846dcd7c19991b7ae1fb4d28c1
Vale Brenes <valebrenes2003@gmail.com>
2023-06-20 16:01:20 -0600

    Actualizacion 4pm

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,20 @@
 // Inserte el código aquí
+let emojis = ["🌸", "🌺", "🌷"]
 
-
-
+let clonarArreglo = function (emojis) {
+    nuevoArreglo = [...emojis]
+    console.log("ajsdf")
+    console.log(nuevoArreglo)
+    return nuevoArreglo
+}
+clonarArreglo(emojis)
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+//export { clonarArreglo, clonarObjeto, sumar, saludar };
+
+let flores = ("")
+
+let clonarObjeto = function () {
+    nuevoArreglo = [...emojis]
+    console.log(nuevoArreglo)
+    return nuevoArreglo
+}
