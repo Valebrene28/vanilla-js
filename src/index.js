caaa03040ddad6c8a59db123072c72a976b73980
Vale Brenes <valebrenes2003@gmail.com>
2023-06-21 15:56:14 -0600

    Actualizacion 4pm

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 // Inserte el código aquí
+function MostrarHora(){
+    var elementoHora= document.getElementById("hora");
+    var fecha = new Date();
+    var hora1 = fecha.getHours()
+    var hora2 = fecha.getMinutes()
+    var hora3 = fecha.getSeconds()
 
+    elementoHora.innerHTML= hora1+ ":"+hora2+ ":"+hora3
+    console.log(hora1, hora2, hora3);
+}
+MostrarHora()
+
+setInterval(MostrarHora, 1000)
