b99d4469b7a9d4e21dcd34b6dcb6bbb99229c569
Vale Brenes <valebrenes2003@gmail.com>
2023-06-22 12:59:44 -0600

    Actualizacion 1pm

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 // Inserte el código aquí
-function MostrarHora(){
-    var elementoHora= document.getElementById("hora");
-    var fecha = new Date();
-    var hora1 = fecha.getHours()
-    var hora2 = fecha.getMinutes()
-    var hora3 = fecha.getSeconds()
+function MostrarHora() {
+  var elementoHora = document.getElementById("hora");
+  var fecha = new Date();
+  var hora1 = fecha.getHours();
+  var hora2 = fecha.getMinutes();
+  var hora3 = fecha.getSeconds();
 
-    elementoHora.innerHTML= hora1+ ":"+hora2+ ":"+hora3
-    console.log(hora1, hora2, hora3);
+  elementoHora.innerHTML = hora1 + ":" + hora2 + ":" + hora3;
 }
-MostrarHora()
+MostrarHora();
 
-setInterval(MostrarHora, 1000)
+setInterval(MostrarHora, 1000);
+
+let usuarios = [];
+fetch("http://localhost:3000/user")
+  .then((data) => data.json())
+  .then((users) => {
+    console.log(users);
+  })
+
+  .catch((error) => {
+    console.log(error);
+    alert("Error en las peticiones");
+  });
