a75528cecfd4a4120a430ca30f7b27441676884f
Vale Brenes <valebrenes2003@gmail.com>
2023-06-22 15:47:13 -0600

    4pm

---
+++ src/calculadora.js
@@ -0,0 +1,18 @@
+const calculadora={
+    sumar: function(a,b) {
+        return a + b
+    },
+    restar: function(a,b) {
+        return a - b
+    },
+    dividir: function(a,b) {
+        return a / b
+    },
+    multiplicar: function(a,b) {
+        return a * b
+    },
+
+}
+
+
+ export{calculadora}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 // Inserte el código aquí
+import { calculadora } from "./calculadora.js";
+function calcular(operacion, numero1, numero2) {
+  let resultado;
+  if (operacion === "sumar") {
+    resultado = calculadora.sumar(numero1, numero2);
+  } else if (operacion === "restar") {
+    resultado = calculadora.restar(numero1, numero2);
+  } else if (operacion === "dividir") {
+    resultado = calculadora.dividir(numero1, numero2);
+  } else if (operacion === "multiplicar") {
+    resultado = calculadora.multiplicar(numero1, numero2);
+  }
 
+  return resultado;
+}
+
+let resultadoSuma = calcular("sumar", 1, 2);
+let resultadoResta = calcular("restar", 5, 2);
+let resultadoMultiplicar = calcular("multiplicar", 3, 2);
+let resultadoDividir = calcular("dividir", 20, 2);
+
+console.log(resultadoMultiplicar);
