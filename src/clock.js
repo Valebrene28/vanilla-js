ae19b5f2c5c125fca7f3377bac7c0480488f4f87
Vale Brenes <valebrenes2003@gmail.com>
2023-06-23 15:59:33 -0600

    4pm

---
+++ src/clock.js
@@ -0,0 +1,13 @@
+function MhoraActual() {
+    let horaHtml= document.getElementById("horaActual");
+   
+   
+       let hora=new Date();
+       let horaH=hora.getHours()
+       let horaM=hora.getMinutes()
+       let horaS=hora.getSeconds()
+   
+       horaHtml.innerHTML=horaH+": "+horaM+": "+horaS
+    }
+     MhoraActual()
+     setInterval(MhoraActual, 1000)
